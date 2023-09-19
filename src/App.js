5cfa7dc60ee43c34196e5123a70cc263f7155b7a
Codeman <naveenlalwani77gmail.com>
2023-09-20 00:12:30 +0530

    +

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [dob, setDob] = useState('');
+  const [age, setAge] = useState(null);
+
+  const calculateAge = async () => {
+    try {
+      const response = await fetch(`http://16.16.117.49:4000/calculate-age?dob=${dob}`);
+      const data = await response.json();
+
+      if (response.status === 200) {
+        setAge(data.age);
+      } else {
+        setAge(null);
+        console.error(data.error);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Age Calculator</h1>
+      <div>
+        <label>Date of Birth (YYYY-MM-DD):</label>
+        <input type="text" value={dob} onChange={(e) => setDob(e.target.value)} />
+        <button onClick={calculateAge}>Calculate Age</button>
+      </div>
+      {age !== null && <p>Your age is: {age} years</p>}
     </div>
   );
 }
